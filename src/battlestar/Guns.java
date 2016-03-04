48cefe4c49b2b1af000794a4f78aec2526f1fe04
Malanius <malanius@seznam.cz>
2016-03-04 19:08:40 +0100

    Battlestar objects
    Added battlestar objects and descriptions

---
+++ src/battlestar/Guns.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Malanius malanius@seznam.cz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package battlestar;
+
+/**
+ * Represents BS long range and flank guns.
+ * The number of guns and crew skill determines attack power.
+ * Guns can be build using ship resources or destroyed in events.
+ * Small craft can't be hit by long range weapons, but can be attacked with flak
+ * when they reach the battlestar.
+ * 
+ * @author Malanius malanius@seznam.cz
+ * @version 0.0.1
+ */
+class Guns {
+    
+}//End of class
---
+++ src/battlestar/Hull.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 Malanius malanius@seznam.cz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package battlestar;
+
+/**
+ * Represents BS hull and structural integrity.
+ * When it's destroyed, it's game over. Hull doesn't provide bonuses to damage
+ * protection, but can be upgraded to provide better structural integrity.
+ * Combat and events damage the hull, but damage can be repaired using resources.
+ * 
+ * @author Malanius malanius@seznam.cz
+ * @version 0.0.1
+ */
+class Hull {
+    
+}//End of class
